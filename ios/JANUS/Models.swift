ad19bea75657c51e715d68a357e35c548ad88433
Vardath <59270526+Vardath@users.noreply.github.com>
2026-08-22 10:05:09 +1000

    Add iOS attachment models

--- ios/JANUS/Models.swift
+++ ios/JANUS/Models.swift
@@ -34,11 +34,36 @@ struct GeneratedImage: Decodable {
     var download_path: String
 }
 
+struct UploadedFile: Identifiable, Decodable {
+    var id: String
+    var filename: String
+    var mime_type: String
+    var size_bytes: Int
+    var sha256: String?
+    var extraction_status: String?
+    var has_extracted_text: Bool?
+    var created_at: Int?
+    var download_path: String?
+}
+
+struct UploadResponse: Decodable {
+    var ok: Bool?
+    var file: UploadedFile
+}
+
+struct ChatRequest: Encodable {
+    var profile_id: String
+    var message: String
+    var attachment_ids: [String]
+}
+
 struct ChatResponse: Decodable {
     var reply: String?
     var response: String?
     var image: GeneratedImage?
     var generated_image: GeneratedImage?
+    var attachments: [UploadedFile]?
+    var attachment_grounding: Bool?
 
     var attachedImage: GeneratedImage? { generated_image ?? image }
 }
