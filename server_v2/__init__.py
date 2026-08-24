87145943cfd9a074290d83b0711e1213e0770282
Vardath <59270526+Vardath@users.noreply.github.com>
2026-08-24 10:12:15 +1000

    Start clean JANUS server v2 package

---
+++ server_v2/__init__.py
@@ -0,0 +1,5 @@
+"""JANUS server reconstruction v2.
+
+This package is intentionally independent of the legacy server composition.
+It preserves JANUS product contracts while rebuilding orchestration from first principles.
+"""
